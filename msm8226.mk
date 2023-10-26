0c26c5df4b0a4ee9f4f7ebc76c384b55737037b6
Bruno Martins <bgcngm@gmail.com>
2023-10-25 18:24:32 -0600

    msm8226-common: Move graphics composer to hwbinder
    The passthrough impl is dead as of Android 11 QPR1.

--- msm8226.mk
+++ msm8226.mk
@@ -69,7 +69,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     copybit.msm8226 \
     gralloc.msm8226 \
