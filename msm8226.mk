10bf93bf89cffb8a5cfb1e82b4eb043d208aed3d
Edwin Wong <edwinwong@google.com>
2023-10-25 19:07:10 -0600

    msm8226-common: Update clearkey plugin to drm HAL v1.4
    update msm8916.mk and change manifest.xml entry to 1.4

--- msm8226.mk
+++ msm8226.mk
@@ -85,7 +85,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.4-service.clearkey
 
 # fastbootd
 PRODUCT_PACKAGES += \
