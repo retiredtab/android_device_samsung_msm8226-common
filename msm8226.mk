f5d3b9c5f1ead4a0a673a6f845fa0bcc1f81f1a9
retiredtab-gerrit <clrh1tablet@gmail.com>
2023-10-25 18:28:53 -0600

    msm8226-common: build OSS power instead of prebuilt proprietary blob

--- msm8226.mk
+++ msm8226.mk
@@ -161,7 +161,8 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
